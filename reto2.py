a0db54d955d24ad1404a566c43dfbdb93bf25dbe
Scooter45 <101779289+Scooter45@users.noreply.github.com>
2022-04-01 23:33:43 -0500

    Add files via upload

--- reto2.py
+++ reto2.py
@@ -2,4 +2,8 @@ n1 = int(input('ingrese la nota 1 (30% de valor):'))
 n2 = int(input('ingrese la nota 2 (30% de valor):'))
 n3 = int(input('ingrese la nota 3 (40% de valor):'))
 nf = ((n1*30)+(n2*30)+(n3*40))/100
-print(nf)
+print(f'la nota final fue:{nf}' ) 
+if nf >= 3:
+    print('el estudiante aprobó')
+else:
+    print('el estudiante reprobó')
